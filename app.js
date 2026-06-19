a1a5e9d92b529c90dba825974dda4ec1d9ec5ade
sheldon-neuse <24fi081@ms.dendai.ac.jp>
2026-06-19 19:57:06 +0900

    update

--- app.js
+++ app.js
@@ -10,10 +10,45 @@ let connects = []
 app.use(express.static('public'))
 
 app.ws('/ws', (ws, req) => {
-  connects.push(ws)
+  connects.push({
+    ws,
+    name: '名無し'
+  })
+
+  broadcastUserCount()
 
   ws.on('message', (message) => {
     console.log('Received:', message)
+
+    const msg = JSON.parse(message)
+
+    // ユーザー名設定
+    if (msg.type === 'username') {
+      const user = connects.find((c) => c.ws === ws)
+
+      if(user) {
+        user.name = msg.name
+      }
+
+      return
+    }
+
+    // チャット
+    if (msg.type === 'chat') {
+      const user = connects.find((c) => c.ws === ws)
+
+      const chatMessage = JSON.stringify({
+        type: 'chat',
+        name: user ? user.name: '名無し',
+        text: msg.text,
+      })
+
+      connects.forEach((client) => {
+        if (client.ws.readyState === 1) {
+          client.ws.send(chatMessage)
+        }
+      })
+    }
 
     connects.forEach((socket) => {
       if (socket.readyState === 1) {
@@ -31,3 +66,16 @@ app.ws('/ws', (ws, req) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
+
+function broadcastUserCount() {
+  const countMessage = JSON.stringify({
+    type: 'count', 
+    count: connects.length,
+  })
+
+  connects.forEach((client) => {
+    if(client.ws.readyState === 1) {
+      client.ws.send(countMessage)
+    }
+  })
+}
